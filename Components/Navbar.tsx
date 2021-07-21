b188b6379ff89678608613a628283fad474be59d
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-07-20 19:12:33 -0500

    convert to ts. Bootstrap and Navbar added/created

---
+++ Components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<>
+			<nav className='navbar navbar-light bg-light'>
+				<div className='container'>
+					<span className='navbar-brand mb-0 h1'>Plaid Practice</span>
+				</div>
+			</nav>
+		</>
+	);
+};
+
+export default Navbar;
