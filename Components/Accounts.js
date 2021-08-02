fd25b098cb500efd305b1f0939fd2d4abde67424
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-08-02 16:41:48 -0500

    changes to accounts.js

--- Components/Accounts.js
+++ Components/Accounts.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import useSWR from 'swr';
+import _ from 'lodash';
+
+import Accordion from 'react-bootstrap/Accordion';
+import Container from 'react-bootstrap/Container';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -8,18 +12,56 @@ const Transactions = () => {
 
 	if (error) return <div>failed to load</div>;
 	if (!data) return <div>No Accounts Yet</div>;
+
+	const { depository, credit, investment, loan } = _.chain(data)
+		.groupBy('type')
+		.value();
+
 	return (
 		<>
 			<h1>Accounts</h1>
 			<br />
-			{data &&
-				data.map(({ name }) => {
-					return (
-						<div key={Math.floor(Math.random() * 1000000)}>
-							<p>{name}</p>
-						</div>
-					);
-				})}
+			<Container className='d-flex justify-content-center'>
+				<Accordion defaultActiveKey='0' className='w-50'>
+					<Accordion.Item eventKey='Cash'>
+						<Accordion.Header>Cash</Accordion.Header>
+						{depository.map(({ account_id, name, balances }) => {
+							return (
+								<>
+									<div
+										key={account_id}
+										style={{
+											display: 'flex',
+											flexDirection: 'row',
+											justifyContent: 'space-between',
+										}}>
+										<Accordion.Body>{name}</Accordion.Body>
+										<Accordion.Body>${balances.current}</Accordion.Body>
+									</div>
+								</>
+							);
+						})}
+					</Accordion.Item>
+					<Accordion.Item eventKey='Credit Cards'>
+						<Accordion.Header>Credit Cards</Accordion.Header>
+						{credit.map(({ account_id, name }) => {
+							return <Accordion.Body key={account_id}>{name}</Accordion.Body>;
+						})}
+					</Accordion.Item>
+					<Accordion.Item eventKey='Investments'>
+						<Accordion.Header>Investments</Accordion.Header>
+						{investment.map(({ account_id, name }) => {
+							return <Accordion.Body key={account_id}>{name}</Accordion.Body>;
+						})}
+					</Accordion.Item>
+					<Accordion.Item eventKey='Loans'>
+						<Accordion.Header>Loans</Accordion.Header>
+						{loan.map(({ account_id, name }) => {
+							return <Accordion.Body key={account_id}>{name}</Accordion.Body>;
+						})}
+					</Accordion.Item>
+				</Accordion>
+			</Container>
 		</>
 	);
 };
